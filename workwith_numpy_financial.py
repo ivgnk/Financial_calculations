727435bb1ca3edbdacba5b8a639409ed41fa5e3d
Ivan Genik <IVGenik@rambler.ru>
2023-09-11 13:00:57 +0500

    Add files via upload

--- workwith_numpy_financial.py
+++ workwith_numpy_financial.py
@@ -22,16 +22,20 @@ ppmt(rate, per, nper, pv[, fv, when]) Compute the payment against loan principal
 rate(nper, pmt, pv, fv[, when, guess, tol, …]) Compute the rate of interest per period.https://numpy.org/numpy-financial/latest/
 '''
 
+import sys
+import inspect
 import numpy as np
 import numpy_financial as npf
 import pprint
+import matplotlib.pyplot as plt
+
 
 def about_numpy_financial():
     pprint.pprint(dir(npf))
 
     pprint.pprint(npf.pv.__doc__)
 
-def test_numpy_financial_fv():
+def thetest_numpy_financial_fv():
     '''
     numpy_financial.fv(rate, nper, pmt, pv, when='end')
     rate - scalar or array_like - Rate of interest as decimal (not per cent) per period
@@ -58,15 +62,36 @@ def test_numpy_financial_fv():
     # By convention, the negative sign represents cash flow out (i.e. money not available today)
     print('total ', npf_fv:=npf.fv(0.05/12, 10*12, -100, -100))
     print('cash additions = ', cash_additions:=100*12*10)
-    print('procents = ', procents:=npf_fv - cash_additions)
+    print('procents = ', procents:=npf_fv - cash_additions-100)
     print('part of procents = ', procents/npf_fv)
-
 
     print('\n2 step')
     a = np.array((0.05, 0.06, 0.07)) / 12
     print(npf.fv(a, 10 * 12, -100, -100))
 
+def thetest_numpy_financial_fv2():
+    print('Функция = ',inspect.currentframe().f_code.co_name,'\n')
+    rate = 0.05/12
+    nper = 10*12
+    pmt = 0
+    pv = -100
+    print('total ', npf_fv:=npf.fv(rate, nper, pmt, pv))
+    print('cash additions = ', cash_additions:=abs(pmt*nper))
+    print('procents = ', procents:=npf_fv - cash_additions-abs(pv))
+    print('part of procents = ', procents/npf_fv)
+    x = np.zeros(nper+1)
+    dat = np.zeros(nper+1)
+    for i in range(nper+1):
+        x[i] = i
+        dat[i]= npf.fv(rate, i, pmt, pv)
+    print(x)
+    plt.plot(x,dat)
+    plt.xlabel('Месяцы')
+    plt.ylabel('Сумма')
+    plt.grid()
+    plt.show()
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # about_numpy_financial()
-    test_numpy_financial_fv()
+    thetest_numpy_financial_fv2()
